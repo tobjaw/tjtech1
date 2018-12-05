5960026bd958517492b7c34ddf94b3ea01cabcaf
tobjaw <tobjaw@gmail.com>
2018-12-06 00:43:51 +0100

    feat: create window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,36 @@
+#include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error (%d): %s\n", error, description);
+}
 
 int main(void)
 {
-    printf("InitGL: simple program is running!\n");
-    return 0;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    printf("glfw init\n");
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "tjtech1", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while (!glfwWindowShouldClose(window)) { glfwPollEvents(); }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
 }
