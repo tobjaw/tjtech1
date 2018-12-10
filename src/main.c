fb55d0ae551ef56a99e2589eeb8de835267c3195
tobjaw <tobjaw@gmail.com>
2018-12-10 03:34:09 +0100

    refactor: util.h init

--- src/main.c
+++ src/main.c
@@ -1,34 +1,13 @@
 #include "main.h"
 
+#include "util.h"
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int ae_load_file_to_memory(const char* filename, char** result)
-{
-    int   size = 0;
-    FILE* f    = fopen(filename, "rb");
-    if (f == NULL)
-    {
-        *result = NULL;
-        return -1;    // -1 means file opening fail
-    }
-    fseek(f, 0, SEEK_END);
-    size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    *result = (char*) malloc(size + 1);
-    if (size != fread(*result, sizeof(char), size, f))
-    {
-        free(*result);
-        return -2;    // -2 means file reading fail
-    }
-    fclose(f);
-    (*result)[size] = 0;
-    return size;
-}
 
 void error_glfw_callback(int error, const char* description)
 {
---
+++ src/util.h
@@ -0,0 +1,4 @@
+/* source: */
+/* http://www.anyexample.com/programming/c/how_to_load_file_into_memory_using_plain_ansi_c_language.xml
+ */
+int ae_load_file_to_memory(const char* filename, char** result);
